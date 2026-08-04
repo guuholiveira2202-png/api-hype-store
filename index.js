61ff77d75ec088055222446722ec584d67b2ea11
guuholiveira2202-png <guuholiveira2202@gmail.com>
2026-08-03 23:36:03 -0300

    atual

--- index.js
+++ index.js
@@ -35,9 +35,9 @@ app.post('/gerar-pagamento', async (req, res) => {
             // VÍNCULO FUNDAMENTAL: Envia o ID do Firebase para o Mercado Pago
             external_reference: idPedido, 
             back_urls: {
-                success: imperiummagnata.com.br,
-                failure: imperiummagnata.com.br,
-                pending: imperiummagnata.com.br,
+                success: SITE_URL,
+                failure: SITE_URL,
+                pending: SITE_URL,
             },
             // Redireciona o cliente automaticamente para o site após aprovação
             auto_return: "approved",
